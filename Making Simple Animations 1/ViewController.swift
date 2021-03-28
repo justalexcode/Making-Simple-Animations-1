2292606d9ac9be706757a92bcc309e695b2fb8cd
Alex <kelalex87@gmail.com>
2021-03-28 11:54:35 +0200

    Initial Commit

---
+++ Making Simple Animations 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Making Simple Animations 1
+//
+//  Created by Alexander  Kelly on 28.03.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

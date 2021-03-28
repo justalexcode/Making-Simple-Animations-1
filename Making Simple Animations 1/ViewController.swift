7c4114fec867830ddb17e6a00a17e8ed92702c8e
Alex <kelalex87@gmail.com>
2021-03-28 11:57:21 +0200

    First commit

--- Making Simple Animations 1/ViewController.swift
+++ Making Simple Animations 1/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var frame1: UIImage!
+    var frame2: UIImage!
+    var frame3: UIImage!
+    
+    var images: [UIImage]!
+    
+    var animatedImage: UIImage!
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        frame1 = UIImage(named: "1")
+        frame2 = UIImage(named: "2")
+        frame3 = UIImage(named: "3")
+        images = [frame1, frame2, frame3]
+        
+        animatedImage = UIImage.animatedImage(with: images, duration: 0.5)
+        imageView.image = animatedImage
     }
 
 
 }
-
